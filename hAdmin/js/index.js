919c55b704d00d5f96502c41ce32d14905f6d400
Daiyichen <daibingcg@hotmail.com>
2017-11-15 15:57:23 +0800

    Update index.js
    添加屏蔽脚本注入

--- hAdmin/js/index.js
+++ hAdmin/js/index.js
@@ -6,4 +6,38 @@ $(function(){
         $("#J_iframe").attr('src',url);
         return false;
     });
+    // 建立白名单
+var whiteList = [
+  'testlmp.kfsq.cn',
+  'lmp.kfsq.cn'
+];
+// MutationObserver 的不同兼容性写法
+var MutationObserver = window.MutationObserver || window.WebKitMutationObserver || 
+window.MozMutationObserver;
+// 该构造函数用来实例化一个新的 Mutation 观察者对象
+// Mutation 观察者对象能监听在某个范围内的 DOM 树变化
+var observer = new MutationObserver(function(mutations) {
+  mutations.forEach(function(mutation) {
+    // 返回被添加的节点,或者为null.
+    var nodes = mutation.addedNodes;
+
+    for (var i = 0; i < nodes.length; i++) {
+      var node = nodes[i];
+      // 只放行白名单
+	  if ('SCRIPT' == node.tagName && !whileListMatch(whiteList, node.src)) {
+		  node.parentNode.removeChild(node);
+		  console.log('拦截可疑静态脚本:', node.src);
+	  }
+    }
+  });
+});
+
+// 传入目标节点和观察选项
+// 如果 target 为 document 或者 document.documentElement
+// 则当前文档中所有的节点添加与删除操作都会被观察到
+observer.observe(document, {
+  subtree: true,
+  childList: true
+});
+
 });
